43de1b3026b99f0cb886b67b6224cb2ad33203ae
Thomas DA SILVA <tda-silv@e2r11p22.clusters.42paris.fr>
2022-03-31 14:36:41 +0200

    mardi 31 mars 2022 - push 28 -

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tda-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 14:11:42 by tda-silv          #+#    #+#             */
-/*   Updated: 2022/03/28 12:06:18 by tda-silv         ###   ########.fr       */
+/*   Updated: 2022/03/31 13:41:41 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ static size_t	ft_setsize_l(char const *s1, char const *set)
 	size_t	i;
 	size_t	j;
 
-	if (!s1)
-		return (0);
 	i = 0;
 	j = 0;
-	while (set[j])
+	while (s1[i] && set[j])
 	{
 		if (s1[i] == set[j])
 		{
@@ -39,16 +37,16 @@ static size_t	ft_setsize_r(char const *s1, char const *set)
 	size_t	i;
 	size_t	j;
 
-	if (!s1)
-		return (0);
 	i = ft_strlen(s1);
-	j = 0;
 	if (i > 0)
 		i--;
-	while (set[j])
+	j = 0;
+	while (s1[i] && set[j])
 	{
 		if (s1[i] == set[j])
 		{
+			if (i == 0)
+				break ;
 			i--;
 			j = 0;
 		}
@@ -60,31 +58,32 @@ static size_t	ft_setsize_r(char const *s1, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*copy;
 	size_t	i;
+	char	*copy;
 
 	if (!s1)
 		return (NULL);
 	if (!set)
 		return (ft_strdup(s1));
-	i = (ft_setsize_r(s1, set) + 1) - ft_setsize_l(s1, set) + 1;
-	if ((ft_setsize_r(s1, set) + 1) < ft_setsize_l(s1, set) + 1)
+	if (ft_setsize_r(s1, set) < ft_setsize_l(s1, set))
 		return (ft_strdup(""));
+	i = ft_setsize_r(s1 + ft_setsize_l(s1, set), set);
+	if (ft_setsize_l(s1, set) == 0 && ft_setsize_r(s1, set) == 0 && !*s1)
+		i += 1;
+	else
+		i += 2;
 	copy = malloc(i);
 	if (!copy)
 		return (NULL);
-	if (s1)
-		ft_strlcpy(copy, s1 + ft_setsize_l(s1, set), i);
-	else
-		copy = 0;
+	ft_strlcpy(copy, s1 + ft_setsize_l(s1, set), i);
 	return (copy);
 }
 /*
-#include <stdio.h>
+//#include <stdio.h>
 
 int	main(void)
 {
-	char	s1[] = "-12-3--";
+	char	s1[] = "---";
 	char	*s11 = ft_strtrim(s1, "-");
 
 	printf("\n%s\n\n", s11);
